365612b639fcd9388597a1da6eaee441157e6c69
jbaptista2 <jbaptista@gobots.com.br>
2021-02-20 20:28:14 -0300

    feat: adding getById, index and store

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 const treinadorController = require('./controllers/TreinadorController');
 
-
+//endpoints para treinadro (crud)
 router.post('/treinador', treinadorController.store);
+router.get('/treinadores', treinadorController.index);
+router.get('/treinador/:id', treinadorController.getById);
+
 
 module.exports = router;
