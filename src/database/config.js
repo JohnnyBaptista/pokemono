1a212ce017fa136e04cb35ce3ebd5fb7039fc828
jbaptista2 <jbaptista@gobots.com.br>
2021-02-21 14:35:37 -0300

    removing values

--- src/database/config.js
+++ src/database/config.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: '157.230.229.199',
-    user: 'root',
-    password: '123batmanFC',
-    database: 'pokemono',
-    port: '3306'
+    host: 'xxxxx',
+    user: 'xxxx',
+    password: 'xxxx',
+    database: 'xxxx',
+    port: 'xxxx'
 });
 
 module.exports = connection;
