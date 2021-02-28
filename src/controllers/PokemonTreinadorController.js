edf2225c1f7595db92dca82eb80d13ed92f52e27
jbaptista2 <jbaptista@gobots.com.br>
2021-02-28 17:23:32 -0300

    feat: pokemon treinador controller

---
+++ src/controllers/PokemonTreinadorController.js
@@ -0,0 +1,66 @@
+const pokemonTreinadorModel = require('../models/PokemonTreinadorModel');
+
+module.exports = {
+    async store(req, res) {
+        try {
+            const { idTreinador, idPokemon } = req.body
+            const response = await pokemonTreinadorModel.insert(idTreinador, idPokemon)
+            if (response) return res.status(200).json({ msg: "Inserido com sucesso!" })
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({ msg: e });
+        }
+    },
+ 
+    async index(req, res) {
+        try {
+            const response = await pokemonTreinadorModel.getAll();
+            if (response) return res.status(200).json({ pokemonTreinador: response })
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({ msg: e });
+        }
+    },
+
+    async getByTrainerId(req, res) {
+        try {
+            const response = await pokemonTreinadorModel.getByTrainerId(req.params.id);
+            if (response.length > 0)
+                return res.status(200).json({ pokemonTreinador: response })
+            else return res.status(200).json({ msg: `Pokemon do treinador com o id: ${req.params.id} nao existe na base`, pokemonTreinador: null });
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({ msg: e });
+        }
+    },
+
+    async getByTrainerId(req, res) {
+        try {
+            const response = await pokemonTreinadorModel.getByPokemonAndTrainerId(req.params.idTreinador, req.params.idPokemon);
+            if (response.length > 0)
+                return res.status(200).json({ pokemonTreinador: response })
+            else return res.status(200).json({ msg: `Ocorreu um erro ao buscar informações com esses id's`, tipoItem: null });
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({ msg: e });
+        }
+    },
+
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+            const item = await pokemonTreinadorModel.getById(id);
+            if (item.length > 0) {
+                const response = await pokemonTreinadorModel.delete(id);
+                if (response) {
+                    return res.status(200).json({ msg: `Item deletado com sucesso!` });
+                } else return res.status(400).json({ item: null, msg: 'FATAL ERROR' });
+            } else {
+                return res.status(400).json({ msg: "Pokemon_treinador com esse ID não existe!" });
+            }
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ msg: e });
+        }
+    }
+}
